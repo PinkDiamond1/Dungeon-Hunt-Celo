5600549fa78b7ad0c068e6e41ecc5f9722c6f328
lopeselio <lopeselio@gmail.com>
2021-07-04 19:50:47 +0530

    Carousel component

--- blockchain/src/components/HeroBooth.js
+++ blockchain/src/components/HeroBooth.js
@@ -1,24 +1,40 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
 import '../styles/booth.css'
-import Header from './Header'
+import Header from './HeroHeader'
 import Box from './Box'
 
 
+
 export default function App() {
+  let [heroes,setHeroes]=useState([]);
+  useEffect(() => {
+    loadData();
+  }, []);
+   const loadData = async () => {
+     const response = await fetch(
+       "https://dungeon-crawler-1.herokuapp.com/v1/character/find/all"
+     );
+     const data = await response.json();
+     setHeroes(data.character);
+     console.log(data);
+   };
   return (
-    <div className="main">
-      <Header title={'Welcome to the Hero Voting Page'} />
+    <div className="main screen">
+      <Header title={"Welcome to the Hero Voting Page"} />
       <div className="body">
         <div class="flex-container">
-         <Box/>
-         <Box/>
-         <Box/>
-         <Box/>
-         <Box/>
-         <Box/>
-
+          {heroes
+            ? heroes.map((hero) => (
+              hero.isStory?null:
+                <Box
+                  img={`https://gateway.pinata.cloud/ipfs/${hero.image}`}
+                  name={hero.name}
+                  walletAddress={hero.walletAddress}
+                />
+              ))
+            : null}
         </div>
       </div>
     </div>
-  )
+  );
 }
