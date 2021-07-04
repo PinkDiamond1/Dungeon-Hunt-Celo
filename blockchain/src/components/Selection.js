daec7e22156040637e673eec298651a9d982d508
lopeselio <lopeselio@gmail.com>
2021-07-04 19:50:28 +0530

    booth and header

--- blockchain/src/components/Selection.js
+++ blockchain/src/components/Selection.js
@@ -1,33 +1,48 @@
-import React from 'react'
-import '../styles/App.css'
-import history from '../history';
-import Angel from '../assets/Angel.png'
-import Castle from '../assets/Castle.png'
+import React from "react";
+import "../styles/App.css";
+import history from "../history";
+import Carousel from "./Carousel";
 
 export default function App() {
   return (
-    <div className="main">
-      <div className="header">
-          <h1>Welcome to the Dungeon Crawler</h1>
-      </div>
+    <div className="main screen">
+      <Carousel/>
+      <div className="carousel-padding header">Welcome to the Dungeon Crawler</div>
       <div className="selection">
-        <div className="select-hero" onClick={() => 
-        {
-          history.push('/HeroBooth')
-          history.go(0);
-        }}>
-            <h4>Vote for Hero</h4>
-            <img src={Angel} alt="Angel" width="200" height="200"/>
+        <div
+          className="select-story"
+          onClick={() => {
+            history.push("/HeroBooth");
+            history.go(0);
+          }}
+        >
+          <h4 className="text-header">Swap ETH/BADGER</h4>
+          <img className="dashboard-card-image" src="https://ipfs.io/ipfs/Qme7QyNBX1Q997Fp1bin8JK9KCpk5hyauae35Aosfp2VxV?filename=Angel.png" 
+          alt="Angel"  />
         </div>
-        <div className="select-story" onClick={() => 
-        {
-          history.push('/StoryBooth')
-          history.go(0);
-        }}>
-        <h4>Vote for Story</h4>
-        <img src={Castle} alt="Angel" width="200" height="200"/>
+        {/* <div
+          className="select-story"
+          onClick={() => {
+            history.push("/StoryBooth");
+            history.go(0);
+          }}
+        >
+          <h4 className="text-header">Vo</h4>
+          <img className="dashboard-card-image" src="https://ipfs.io/ipfs/QmemTF7ziTVBFFpZzqYvSF3hdK5q6J2bZ85FYUr4mG2CUw?filename=Castle.png" 
+          alt="Castel"  />
+        </div> */}
+        <div
+          className="select-story"
+          onClick={() => {
+            history.push("/App");
+            history.go(0);
+          }}
+        >
+          <h4 className="text-header">Go to your Inventory</h4>
+          <img className="dashboard-card-image" src="https://ipfs.io/ipfs/QmccVpftx3x3W7GxTQGkJjXS6VyJCpgDgHagkAoJCsLnXM?filename=home.png" 
+          alt="home"  />
         </div>
       </div>
     </div>
-  )
+  );
 }
