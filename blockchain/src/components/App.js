1d80e6860c105a6ee6ea085e82fc15cd2aa17ef4
lopeselio <lopeselio@gmail.com>
2021-07-04 19:50:08 +0530

    App and box component

--- blockchain/src/components/App.js
+++ blockchain/src/components/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 // import logo from '../logo.png';
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import HeaderNFT from './HeaderNFT'
+
 import './App.css';
 import Web3 from 'web3';
-import GladiatorCharacter from '../build/contracts/GladiatorCharacter.json'
+import { Spinner } from 'reactstrap';
+import { Image } from 'react-bootstrap'
+import ChainlinkLogo from '../assets/chainlink.png'
+
+import TropyChar from '../abis/TrophyChar.json'
+import DungeonToken from '../abis/DungeonToken.json'
 
 class App extends Component {
 
@@ -17,9 +21,10 @@ class App extends Component {
       isLoggedIn: false,
       // items: [], 
       account: '',
-      contract: null, 
-      totalSupply: 0,
-      trophies: []
+      contract: null,
+      usersNftCount: 0,
+      trophies: [],
+      loading: true
     };
     this.loadWeb3();
     this.loadBlockchainData();
@@ -41,35 +46,40 @@ class App extends Component {
   async loadBlockchainData() {
     const web3 = window.web3
     const accounts = await web3.eth.getAccounts()
-    this.setState({account: accounts[0]})
+    this.setState({ account: accounts[0] })
 
     const networkId = await web3.eth.net.getId()
-    const networkData = GladiatorCharacter.networks[networkId]
-    if(networkData){
-      const abi = []
+    const networkData = TropyChar.networks[networkId]
+    if (networkData) {
+      const abi = TropyChar.abi;
       const address = networkData.address
       const contract = new web3.eth.Contract(abi, address)
-      // console.log(contract)
+      const contract2 = new web3.eth.Contract(DungeonToken.abi, DungeonToken.networks[networkId].address)
+      //console.log(this.state.account)
       this.setState({ contract })
-      const totalSupply = await contract.methods.totalSupply().call()
-      this.setState({totalSupply})
-      for(var i = 1; i<= totalSupply; i++) {
-        const trophy = await contract.methods.trophies(i-1).call()
+      this.setState({ contract2 })
+
+
+      const usersNftCount = await contract.methods.usersNftCount(this.state.account).call()
+
+      this.setState({ usersNftCount: Number(usersNftCount.toString()) })
+
+      console.log(usersNftCount.toString());
+
+      for (var i = 1; i <= this.state.usersNftCount; i++) {
+        const trophy = await contract.methods.usersNft(this.state.account, i).call()
+        //console.log(trophy);
         this.setState({
           trophies: [...this.state.trophies, trophy]
         })
-
       }
-      console.log(this.state.trophies)
 
-      // Load trophies
-
+      //console.log('users trophies', this.state.usersNftCount, this.state.trophies)
     } else {
       window.alert('Smart contract not deployed to the detected network')
     }
-    
   }
-  
+
 
   render() {
     const isLoggedIn = this.state.isLoggedIn
@@ -78,118 +88,104 @@ class App extends Component {
     const isRewarded = this.state.isRewarded
     return (
       <div className="screen">
+        
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="#"
+            className="navbar-brand col-sm-3 col-md-2 mr-0 text-header"
+            href=""
             target="_blank"
             rel="noopener noreferrer"
           >
-            Dungeons Game
+            The Dungeon Crawler
           </a>
           <ul className='navbar-nav px-3'>
             <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
-              <small className='text-white'><span id='account'>{this.state.account}</span></small>
+              <small className='text-white'><span id='account' className='text-header1'>{this.state.account}</span></small>
             </li>
           </ul>
         </nav>
-        <div className='nft-sec'>
-          <div className="dungeon">
-            <div className="avatar-section">
-              <Container>
-                <Row>
-                  <Col sm={8}>
-                    <div>
-                      {avatar(isLoggedIn)}
-                    </div>
-                  </Col>
-                  <Col sm={4}>
-                    <div>
-                      {mint(isMinting)}
-                    </div>
-                  </Col>
+        
+        <div className='nft-sec container-fluid mt-5'>
+          <HeaderNFT />
+          <div className='row'>
+            <main role='main' className='col-lg-12 d-flex text-center'>
+              <div className="trophy-section-header">
+                <h5 className="trophy-title"><span>  </span>Your Trophy Cabinet <span> </span></h5>
+                <h6 className="text-header1 white">Powered by <img src={ChainlinkLogo} className='chainlink' /> <a className="hovero" href="https://docs.chain.link/docs/chainlink-vrf"><span className="hovero">Chainlink VRF</span></a>  </h6>
+                {/* <h5 className="text col-md-6 col-sm">Your Trophy Cabinet</h5> </div> */}
+                {
+                  // this.state.loading ? (
+                  // <Spinner className='spinner' color="info" />
+                  // <Spinner color="info" />
+                  // ):
+                  <button
+                    type="submit"
+                    className="btn btn-link btn-block btn-sm mint-btn dispToken"
+                    onClick={(event) => {
+                      event.preventDefault()
+                      console.log('triggered');
+                      this.state.contract.methods.requestNewRandomTrophy(
+                        1,
+                        'sagar',
+                        1,
+                        // '0x775C72FB1C28c46F5E9976FFa08F348298fBCEC0'
+                        this.state.account
+                      ).send({ from: this.state.account }).on('transactionHash', (hash) => {
+                        //console.log('true');
+                        this.setState({ loading: false })
+                      })
 
-                </Row>
-              </Container>
-            </div>
-            
-            <div className='trophy-cabinet'>
-              <div className="title">
-                <span className="text">Your Trophy Cabinet</span>
-              </div>  
-              <div className="scroll">           
-                <div class="container vertical-scrollable">
-                  <div class="row text-center">
-                      <div>{box(isRewarded)}</div>
-                      <div>{box(isRewarded)}</div>
-                      <div>{box(isRewarded)}</div>
-                      <div>{box(isRewarded)}</div>
-                      <div>{box(isRewarded)}</div>
-                      <div>{box(isRewarded)}</div>
-                      <div>{box(isRewarded)}</div>
-                      <div>{box(isRewarded)}</div>
-                      <div>{box(isRewarded)}</div>
-                      <div>{box(isRewarded)}</div>
-                      <div>{box(isRewarded)}</div>
-                      <div>{box(isRewarded)}</div>
-                      <div>{box(isRewarded)}</div>
-                      <div>{box(isRewarded)}</div>
-                      <div>{box(isRewarded)}</div>
-                      <div>{box(isRewarded)}</div>
-                      <div>{box(isRewarded)}</div>
-                      <div>{box(isRewarded)}</div>
+                      // this.state.contract2.methods.reward(
+                      //   this.state.account
+                      // ).send({ from: this.state.account }).on('transactionHash', (hash) => {
+                      //   //console.log('true');
+                      //   this.setState({ loading: false })
+                      // })
+                    }}>
+                    Test Mint Random NFT...
+                </button>
 
-                  </div>
-                </div>
-              </div> 
-            </div>
+                }
+
+              </div>
+            </main>
           </div>
-        </div>        
-      </div>
+          <hr /><hr />
+
+          <div className="dungeon trophy-cabinet row text-center">
+
+            {/* <div className='row trophy-cabine'> */}
+            {this.state.trophies.map((trophy, key) => {
+              //console.log(trophy, key);
+              let levelPass = trophy.levelPass.toString()
+              let name = trophy.name.toString()
+              let tokenId = trophy.tokenId.toString()
+              let rarity = trophy.rarity.toString()
+              let worth = trophy.worth.toString()
+
+              const nftMint = ["https://ipfs.io/ipfs/QmPbBTESpMSsGjKisM73deE3PLqA76s2zh1nHhvAkAfYf4?filename=btc.png", "https://ipfs.io/ipfs/Qmb3jBv3xdDettAQokTxQc5T4G1buxY9oxRiSA9YepeRrP?filename=crystal.png", "https://ipfs.io/ipfs/QmZg13ohhyY9xBYnhF1XbAm8qjW43SjxDXsXTyTGFkezWX?filename=chest.png", "https://ipfs.io/ipfs/QmNyZd4czMAY8rxYjGK6b8SR69m2W9FHbkVsnCJstewkXY?filename=god.png", "https://ipfs.io/ipfs/QmXHYB8eEpEQjZq6Hc9vCHdPGwpHPjZfoRYQNwqNZVsKQ8?filename=diamond.png"]
+              const random = Math.floor(Math.random() * nftMint.length);
+              console.log(random, nftMint[random]);
+              console.log(levelPass, name, tokenId, rarity, worth);
+              return (
+                <div key={key} className='gift'>
+                  <div className="giftBox">
+                    <Image src={nftMint[random]} roundedCircle style={{ height: 100, width: 100, alignSelf: 'center', justifyContent: 'center' }} />
+                  </div>
+                  <span className="trophy-info">Level: <span className="values">{levelPass}</span></span><br />
+                  <span className="trophy-info">tokenID: <span className="values">{tokenId}</span></span><br />
+                  <span className="trophy-info">Rarity: <span className="values">{rarity}</span></span><br />
+                  <span className="trophy-info">Worth: <span className="values">{worth}</span></span><br />
+                </div>
+              )
+            })}
+
+
+            {/* </div> */}
+          </div>
+        </div >
+      </div >
     );
   }
 }
-
-function box(props) {
-  const isRewarded = props.isRewarded;
-  if (isRewarded) {
-    return (
-      <div className='drawer col-xs-4'></div>
-    )
-
-  }else {
-  return (
-    <div className='drawer col-xs-4'>NFT</div>
-  );
-  }
-}
-
-function avatar(props) {
-  const isLoggedIn= props.isLoggedIn;
-  if (isLoggedIn) {
-    return (
-      <div className='avatar'></div>
-    )
-
-  }else {
-  return (
-    <div className='avatar'>AVATAR</div>
-  );
-  }
-}
-
-function mint(props) {
-  const isMinting= props.isMinting;
-  if (isMinting) {
-    return (
-      <div className='minted'></div>
-    )
-
-  }else {
-  return (
-    <div className='minted'>YOUR NEW NFT</div>
-  );
-  }
-}
-
-export default App;
+export default App
