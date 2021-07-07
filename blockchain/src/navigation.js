be8ed2014ea8bef8f874e570ce5f9b7ccf70dda2
lopeselio <lopeselio@gmail.com>
2021-07-07 12:26:02 +0530

    fixed route

--- blockchain/src/navigation.js
+++ blockchain/src/navigation.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import history from './history';
 
@@ -7,19 +7,27 @@ import Upload from './components/Upload';
 import Login from './components/Login';
 import TheExchange from './components/SwapExchange/SwapExchange';
 import StoryBooth from './components/StoryBooth';
+import App from "./components/App";
+import { UserContext } from './utils/UserContext'
 
 
 
 export default function Routes() {
+    const [user, setUser] = useState(undefined)
     return (
-        <BrowserRouter history={history} forceRefresh={true}>
+        <BrowserRouter history={history}>
+            <UserContext.Provider value={{user, setUser}}>
             <Switch>
                 <Route path="/" exact component={Selection} />
+                <Route path="/App" exact component={App} />
                 <Route path="/Upload" exact component={Upload} />
                 <Route path="/Login" exact component={Login} />
                 <Route path="/TheExchange" exact component={TheExchange} />
                 <Route path="/StoryBooth" exact component={StoryBooth} />
+                
+
             </Switch>
+            </UserContext.Provider>
         </BrowserRouter>
     )
 
